7c7a51653dddc4f1d8affaece9c80c8e59b28091
tosu1010 <ikiblea10@gmail.com>
2019-09-21 13:34:32 +0900

    main-notify-todo.scssの作成

--- app/assets/javascripts/mypage/tab_change.js
+++ app/assets/javascripts/mypage/tab_change.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.mypage__main__tab > li').on('click', function(){
-    $('.mypage__main__tab > li').removeClass('active');
+    $(this).siblings().removeClass('active');
     $(this).addClass('active');
   });
 });
