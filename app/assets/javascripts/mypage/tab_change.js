668bb002672017e84046f27a502a8379d6a943be
tosu1010 <ikiblea10@gmail.com>
2019-09-21 16:04:11 +0900

    not-found.scssの作成
    該当のタブの要素が何も見つからなかった場合に表示する文言、画像を指定

--- app/assets/javascripts/mypage/tab_change.js
+++ app/assets/javascripts/mypage/tab_change.js
@@ -2,5 +2,9 @@ $(function(){
   $('.mypage__main__tab > li').on('click', function(){
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
+    
+    let index = $('.mypage__main__tab > li').index(this);
+    $('.mypage__main__tab-content > ul').eq(index).siblings().removeClass('show');
+    $('.mypage__main__tab-content > ul').eq(index).addClass('show');
   });
 });
