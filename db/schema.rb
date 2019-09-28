f25c4c28b7f3b3c64fcdb36a29670b1aed4459a2
tosu1010 <ikiblea10@gmail.com>
2019-09-28 16:32:40 +0900

    Personal_infomationテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_070800) do
+ActiveRecord::Schema.define(version: 2019_09_28_071223) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2019_09_28_070800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "personal_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code"
+    t.string "prefecture"
+    t.string "city"
+    t.string "number"
+    t.string "building"
+    t.string "identification"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_personal_informations_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,4 +60,5 @@ ActiveRecord::Schema.define(version: 2019_09_28_070800) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "personal_informations", "users"
 end
