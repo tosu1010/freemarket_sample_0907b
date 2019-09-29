f6128cc942cc98dcf37228af457774da09b6d62e
tosu1010 <ikiblea10@gmail.com>
2019-09-29 12:36:30 +0900

    ancestryカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_105211) do
+ActiveRecord::Schema.define(version: 2019_09_29_033425) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_105211) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
