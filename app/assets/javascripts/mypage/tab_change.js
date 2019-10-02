d6f66d5ba699a4d5b77043a5389d6e8d9b94026a
tosu1010 <ikiblea10@gmail.com>
2019-10-03 00:17:56 +0900

    リファクタリング
    各種ファイルのリファクタリングを実行

--- app/assets/javascripts/mypage/tab_change.js
+++ app/assets/javascripts/mypage/tab_change.js
@@ -1,10 +1,10 @@
 $(function(){
-  $('.mypage__main__tab > li').on('click', function(){
+  $('.tab > li').on('click', function(){
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
     
-    let index = $('.mypage__main__tab > li').index(this);
-    $('.mypage__main__tab-content > ul').eq(index).siblings().removeClass('show');
-    $('.mypage__main__tab-content > ul').eq(index).addClass('show');
+    let index = $('.tab > li').index(this);
+    $('.tab-content > ul').eq(index).siblings().removeClass('show');
+    $('.tab-content > ul').eq(index).addClass('show');
   });
 });
