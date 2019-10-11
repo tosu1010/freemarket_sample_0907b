c8d42ec30c01c3a2a1df08a59ee9c65e8937d8ac
chokobo-ume <@users.noreply.github.com>
2019-10-12 02:14:30 +0900

    routesとcontrollerの仮設定

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,22 @@
+class SignupController < ApplicationController
+
+
+  def sign_up_before
+  end
+
+  def sign_up_2
+  end
+
+  def sign_up_3
+  end
+
+  def sign_up_4
+  end
+
+  def sign_up_5
+  end
+
+  def sign_up_6
+  end
+
+end
