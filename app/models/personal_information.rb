1246fe5c4d3da6bcfe1a3de26fa0f7835f7c33ae
chokobo-ume <@users.noreply.github.com>
2019-10-20 17:35:20 +0900

    active_hashの追加と関連viewの編集

--- app/models/personal_information.rb
+++ app/models/personal_information.rb
@@ -1,4 +1,7 @@
 class PersonalInformation < ApplicationRecord
   has_one :remittee, dependent: :destroy
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
