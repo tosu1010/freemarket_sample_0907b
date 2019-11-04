409fa89b234993aedbb5a9b85abf4cabd25b6771
tosu1010 <ikiblea10@gmail.com>
2019-11-04 16:24:05 +0900

    Activehashのextendの追加

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -3,5 +3,7 @@ class Exhibit < ApplicationRecord
   has_many :exhibit_images, dependent: :destroy
   belongs_to :user
   belongs_to :merchandise
-  belongs_to_active_hash :size
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :size
 end
