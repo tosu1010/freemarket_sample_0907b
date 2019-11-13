6d90992587378fb5fd6ba9d424194c193500fa2e
tosu1010 <ikiblea10@gmail.com>
2019-11-13 13:49:20 +0900

    商品情報及びユーザー情報の取得
    DBから値を拾ってくるように修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,7 +1,8 @@
 class PurchaseController < ApplicationController
 
   def index
-    
+    # TODO 商品詳細バックエンドマージ後、findの部分をparams[merchandise.id]へ
+    @merchandise = Merchandise.find(1)
   end
 
   def create
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
 
+  def get_full_address
+    self.prefecture.name + self.city + self.number
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
