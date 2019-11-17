dd5577c5d2ee60fac04c26b8ad485820c45d00d4
chokobo-ume <@users.noreply.github.com>
2019-11-17 18:25:51 +0900

    userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,21 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :purchases, dependent: :destroy
   has_many :evaluations, dependent: :destroy
+
+  # step1入力項目
+  validates :nickname,                presence: true, length: {maximum: 20}
+  validates :email,                   presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,                presence: true, length: {minimum: 7, maximum: 128}
+  validates :password_confirmation,   presence: true, length: {minimum: 7, maximum: 128}
+  validates :last_name,               presence: true
+  validates :first_name,              presence: true
+  validates :last_name_kana,          presence: true
+  validates :first_name_kana,         presence: true
+  validates :birth_year,              presence: true
+  validates :birth_month,             presence: true
+  validates :birth_day,               presence: true
+
+  # step2入力項目
+  validates :phone_number,            presence: true, uniqueness: true, length: {minimum: 10, maximum: 11}, format: { with: /\A\d{10,11}\z/ }, on: :validates_step2
+
 end
