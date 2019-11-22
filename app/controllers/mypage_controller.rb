2c27d025f6501feb6a98f6178dfde42eb3b84cea
chokobo-ume <@users.noreply.github.com>
2019-11-23 02:44:28 +0900

    プロフィールの更新用controller

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,22 +1,39 @@
 class MypageController < ApplicationController
+
+  before_action :move_to_root_path
+
   def index
     
   end
 
   def edit
-    
-  end
-
-  def new
-
+    @user = User.find(current_user.id)
   end
 
   def show
 
   end
 
-  def create
-
+  def card
   end
 
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:alert] = "変更しました"
+      redirect_to mypage_profile_path
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:nickname, :profile_description)
+  end
+
+  def move_to_root_path
+    redirect_to root_path unless user_signed_in?
+  end
 end
