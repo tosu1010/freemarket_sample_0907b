80f3a37c31783be43bfb56f300dfc5248e398cfd
tosu1010 <ikiblea10@gmail.com>
2019-12-07 13:02:57 +0900

    マイページ周辺の修正
    ・リンクの修正
    ・メニューの現在いるページの取得(side.js)

--- app/assets/javascripts/side/side.js
+++ app/assets/javascripts/side/side.js
@@ -3,12 +3,17 @@ $(function(){
   //現在のパスを取得
   var path = location.pathname
 
-  //ログアウト画面の場合
-  if (path.match(/^\w*\/logout/)){
-    $('.menu__links__box').last().css({
-      'background' : '#eee',
-      'font-weight' : '600'
-    })
-  };
-  
+  // 現在のパスがメニューのリンクと一致すれば、背景色を変更する
+  $('.menu__links__box__link').each(function(index, elem){
+    if(path == elem.getAttribute('href')){
+      $(elem).css({
+        'background' : '#eee',
+        'font-weight' : '600'
+      })
+      $(elem).find('.menu__links__box__angle').css({
+        'color': '#333',
+        'font-size': '1.2em'
+      })
+    }
+  })
 });
