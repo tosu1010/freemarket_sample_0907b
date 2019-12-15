8d94c93c1da8c73c998d9c37963e55227aa5d00f
tosu1010 <ikiblea10@gmail.com>
2019-12-15 12:01:09 +0900

    previewとphotoのjsの修正
    各数字ごとに別れていたプレビュー部分を、引数によって分けられるように修正

--- app/assets/javascripts/exhibit/exhibit.js
+++ app/assets/javascripts/exhibit/exhibit.js
@@ -1,30 +1,35 @@
-// $(document).on('turbolinks:load', function() {
-    
-  /////出品画像表示/////
-  /////1:1/////
-    let exhibit_photo1 =
-      `
-      <div class="img-view">
-        <div class="img-field">
-          <div class="img-field-upper">
-            <img class="preview">
-          </div>
-          <div class="img-field-bottom">
-            <label class="change">
-              <div class="edit-btn">編集</div>
-              <button id="clear">削除</button>
-            </label>
-          </div>
+  // preview部分の作成
+  // 引数に数字を渡すことでクラスに適用
+  function buildPhotoHtml(photo_num) {
+    let exhibit_photo = 
+    `
+    <div class="img-view${photo_num}">
+      <div class="img-field">
+        <div class="img-field-upper">
+          <img class="preview${photo_num}">
+        </div>
+        <div class="img-field-bottom">
+          <label class="change">
+            <div class="edit-btn">編集</div>
+            <button id="clear${photo_num}">削除</button>
+          </label>
         </div>
       </div>
-      `
-  
-  /////select_1/////
-    let select_1 =
+    </div>
     `
-    <input class="select_img" type="file" id="select_img" name="images[]" multiple>
-    <div class="select_1">
-      <label class="content__box__dropbox" for="select_img">
+
+    return exhibit_photo
+  }
+
+
+  // selectフォームの部分の作成(画像投稿部分)
+  // preview同様、引数に数字を渡すことでクラスに適用
+  function buildSelectHtml(select_num) {
+    let select = 
+    `
+    <input class="select_img_${select_num}" type="file" id="select_img_${select_num}" name="images[]" multiple>
+    <div class="select_${select_num}">
+      <label class="content__box__dropbox__${select_num}" for="select_img_${select_num}">
         <div class="content__box__dropbox__form">
           ドラッグアンドドロップ
           <br>
@@ -33,124 +38,10 @@
       </label>
     </div>
     `
-  
-  /////2:1/////
-    let exhibit_photo2 =
-    `
-    <div class="img-view2">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview2">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear2">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_2/////
-    let select_2 =
-    `
-    <input class="select_img_2" id="select_img_2" type="file" name="images[]", multiple>
-    <div class="select_2">
-      <label class= "content__box__dropbox__2" for="select_img_2">
-        <div class= "content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////3:1/////
-    let exhibit_photo3 =
-    `
-    <div class="img-view3">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview3">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear3">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_3/////
-    let select_3 =
-    `
-    <input class="select_img_3" id="select_img_3" type="file" name="images[]", multiple>
-    <div class="select_3">
-    <label class= "content__box__dropbox__3" for="select_img_3">
-        <div class= "content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////4:1/////
-    let exhibit_photo4 =
-    `
-    <div class="img-view4">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview4">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <input class="change_select_img" type="file" id="change_select_img"/>
-            <div class="edit-btn">編集</div>
-            <button id="clear4">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_4/////
-    let select_4 =
-    `
-    <input class="select_img_4" id="select_img_4" type="file" name="images[]", multiple>
-    <div class="select_4">
-      <label class= "content__box__dropbox__4" for="select_img_4">
-        <div class= "content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////5:1/////
-    let exhibit_photo5 =
-    `
-    <div class="img-view5">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview5">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear5">削除</button>
-            </label>
-        </div>
-      </div>
-    </div>
-    `
+    return select
+  }
+
+  /////出品画像表示/////
   
     let exhibit_photo5_2 =
     `
@@ -167,188 +58,7 @@
       </div>
     </div>
     `
-  
-  
-  /////select_5/////
-    let select_5 =
-    `
-    <input class="select_img_5" id="select_img_5" type="file" name="images[]", multiple= true/>
-    <div class="select_5">
-      <label class="content__box__dropbox__5" for="select_img_5">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-    
-  /////6:1/////
-    let exhibit_photo6 =
-    `
-    <div class="img-view6">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview6">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear6">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_6/////
-    let select_6 =
-    `
-    <input class="select_img_6" id="select_img_6" type="file" name="images[]", multiple>
-    <div class="select_6">
-      <label class="content__box__dropbox__6" for="select_img_6">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////7:1/////
-    let exhibit_photo7 =
-    `
-    <div class="img-view7">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview7">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear7">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_7/////
-    let select_7 =
-    `
-    <input class="select_img_7" id="select_img_7" type="file" name="images[]", multiple>
-    <div class="select_7">
-      <label class="content__box__dropbox__7" for="select_img_7">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////8:1/////
-    let exhibit_photo8 =
-    `
-    <div class="img-view8">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview8">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear8">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_8/////
-    let select_8 =
-    `
-    <input class="select_img_8" id="select_img_8" type="file" name="images[]", multiple>
-    <div class="select_8">
-      <label class="content__box__dropbox__8" for="select_img_8">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////9:1/////
-    let exhibit_photo9 =
-    `
-    <div class="img-view9">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview9">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear9">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_9/////
-    let select_9 =
-    `
-    <input class="select_img_9" id="select_img_9" type="file" name="images[]", multiple>
-    <div class="select_9">
-      <label class="content__box__dropbox__9" for="select_img_9">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
-  /////10/////
-    let exhibit_photo10 =
-    `
-    <div class="img-view10">
-      <div class="img-field">
-        <div class="img-field-upper">
-          <img class="preview10">
-        </div>
-        <div class="img-field-bottom">
-          <label class="change">
-            <div class="edit-btn">編集</div>
-            <button id="clear10">削除</button>
-          </label>
-        </div>
-      </div>
-    </div>
-    `
-  
-  /////select_10/////
-    let select_10 =
-    `
-    <input class="select_img_10" id="select_img_10" type="file" name="images[]", multiple= true/>
-    <div class="select_10">
-      <label class="content__box__dropbox__10" for="select_img_10">
-        <div class="content__box__dropbox__form">
-          ドラッグアンドドロップ
-          <br>
-          またはクリックしてファイルをアップロード
-        </div>
-      </label>
-    </div>
-    `
-  
+
   // カテゴリーの各オプションの作成
   function buildOption(category){
     let html = `
@@ -586,12 +296,12 @@
   
   
   /////1:1/////
-  $(document).on('change', '#select_img', function (e) {
+  $(document).on('change', '#select_img_1', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_1").replaceWith(exhibit_photo1);
-      $(".img-view").after(select_2);
-      $(".preview").attr('src', e.target.result);
+      $(".select_1").replaceWith(buildPhotoHtml('1'));
+      $(".img-view1").after(buildSelectHtml('2'));
+      $(".preview1").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
   });
@@ -600,8 +310,8 @@
   $(document).on('change', '#select_img_2', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_2").replaceWith(exhibit_photo2);
-      $(".img-view2").after(select_3);
+      $(".select_2").replaceWith(buildPhotoHtml('2'));
+      $(".img-view2").after(buildSelectHtml('3'));
       $(".preview2").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -611,8 +321,8 @@
   $(document).on('change', '#select_img_3', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_3").replaceWith(exhibit_photo3);
-      $(".img-view3").after(select_4);
+      $(".select_3").replaceWith(buildPhotoHtml('3'));
+      $(".img-view3").after(buildSelectHtml('4'));
       $(".preview3").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -622,8 +332,8 @@
   $(document).on('change', '#select_img_4', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_4").replaceWith(exhibit_photo4);
-      $(".img-view4").after(select_5);
+      $(".select_4").replaceWith(buildPhotoHtml('4'));
+      $(".img-view4").after(buildSelectHtml('5'));
       $(".preview4").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -633,7 +343,7 @@
   $(document).on('change', '#select_img_5', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_5").replaceWith(exhibit_photo5);
+      $(".select_5").replaceWith(buildPhotoHtml('5'));
       $(".content__box__img").append(exhibit_photo5_2);
       $(".preview5").attr('src', e.target.result);
     }
@@ -644,8 +354,8 @@
   $(document).on('change', '#select_img_6', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_6").replaceWith(exhibit_photo6);
-      $(".img-view6").after(select_7);
+      $(".select_6").replaceWith(buildPhotoHtml('6'));
+      $(".img-view6").after(buildSelectHtml('7'));
       $(".preview6").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -655,8 +365,8 @@
   $(document).on('change', '#select_img_7', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_7").replaceWith(exhibit_photo7);
-      $(".img-view7").after(select_8);
+      $(".select_7").replaceWith(buildPhotoHtml('7'));
+      $(".img-view7").after(buildSelectHtml('8'));
       $(".preview7").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -666,8 +376,8 @@
   $(document).on('change', '#select_img_8', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_8").replaceWith(exhibit_photo8);
-      $(".img-view8").after(select_9);
+      $(".select_8").replaceWith(buildPhotoHtml('8'));
+      $(".img-view8").after(buildSelectHtml('9'));
       $(".preview8").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -677,8 +387,8 @@
   $(document).on('change', '#select_img_9', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_9").replaceWith(exhibit_photo9);
-      $(".img-view9").after(select_10);
+      $(".select_9").replaceWith(buildPhotoHtml('9'));
+      $(".img-view9").after(buildSelectHtml('10'));
       $(".preview9").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -688,7 +398,7 @@
   $(document).on('change', '#select_img_10', function (e) {
     let reader = new FileReader();
     reader.onload = function (e) {
-      $(".select_10").replaceWith(exhibit_photo10);
+      $(".select_10").replaceWith(buildPhotoHtml('10'));
       $(".preview10").attr('src', e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
@@ -696,63 +406,63 @@
       
   
   /////削除機能/////
-  $(document).on("click", "#clear", function(){
-    $(".img-view").remove();
-    $("#select_img").remove();
-    $(".select_2").replaceWith(select_1)
+  $(document).on("click", "#clear1", function(){
+    $(".img-view1").remove();
+    $("#select_img_1").remove();
+    $(".select_2").replaceWith(buildSelectHtml('1'));
   });
 
   $(document).on("click", "#clear2", function(){
     $(".img-view2").remove();
     $("#select_img_2").remove();
-    $(".select_3").replaceWith(select_2);
+    $(".select_3").replaceWith(buildSelectHtml('2'));
   });
   
   $(document).on("click", "#clear3", function(){
     $(".img-view3").remove();
     $("#select_img_3").remove();
-    $(".select_4").replaceWith(select_3)
+    $(".select_4").replaceWith(buildSelectHtml('3'))
   });
 
   $(document).on("click", "#clear4", function(){
     $(".img-view4").remove();
     $("#select_img_4").remove();
-    $(".select_5").replaceWith(select_4)
+    $(".select_5").replaceWith(buildSelectHtml('4'))
   });
 
   $(document).on("click", "#clear5", function(){
     $(".img-view5").remove();
     $("#select_img_5").remove();
     $(".content__box__select-img-2").remove();
-    $(".img-view4").after(select_5);
+    $(".img-view4").after(buildSelectHtml('5'));
   });
 
   $(document).on("click", "#clear6", function(){
     $(".img-view6").remove();
     $("#select_img_6").remove();
-    $(".select_7").replaceWith(select_6)
+    $(".select_7").replaceWith(buildSelectHtml('6'))
   });
 
   $(document).on("click", "#clear7", function(){
     $(".img-view7").remove();
     $("#select_img_7").remove();
-    $(".select_8").replaceWith(select_7)
+    $(".select_8").replaceWith(buildSelectHtml('7'))
   });
 
   $(document).on("click", "#clear8", function(){
     $(".img-view8").remove();
     $("#select_img_8").remove();
-    $(".select_9").replaceWith(select_8)
+    $(".select_9").replaceWith(buildSelectHtml('8'))
   });
 
   $(document).on("click", "#clear9", function(){
     $(".img-view9").remove();
     $("#select_img_9").remove();
-    $(".select_10").replaceWith(select_9)
+    $(".select_10").replaceWith(buildSelectHtml('9'))
   });
 
   $(document).on("click", "#clear10", function(){
     $("#select_img_10").remove();
-    $(".img-view10").replaceWith(select_10);
+    $(".img-view10").replaceWith(buildSelectHtml('10'));
 
   });
