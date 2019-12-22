e232fa330327a7c1055ace55ae189caa332faba1
tosu1010 <ikiblea10@gmail.com>
2019-12-22 15:17:07 +0900

    出品中の商品の表示

--- app/controllers/exhibit_list_controller.rb
+++ app/controllers/exhibit_list_controller.rb
@@ -1,17 +1,17 @@
-class ExhibitListController < ApplicationControlle
+class ExhibitListController < ApplicationController
 
   # 出品中の商品
   def list
-    
+    @exhibit_list = Exhibit.where(user_id: current_user.id, status: 1)
   end
 
   # 取引中の商品
   def dealing
-
+    @exhibit_list = Exhibit.where(user_id: current_user.id, status: 2)
   end
 
   # 売却済みの商品
-  def compleate
-
+  def complete
+    @exhibit_list = Exhibit.where(user_id: current_user.id, status: 3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
   
-  resources :mypage, only: [:index]
   resources :credit_card, only: [:create]
   resources :merchandises, only: [:index, :show] do
     resources :comments, only: [:create, :new]
@@ -32,14 +31,19 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :mypage, only: [:index]
   get '/mypage/profile' => 'mypage#edit'
   get '/mypage/card' => 'mypage#show'
   get '/mypage/card/create' => 'mypage#create'
   patch '/mypage' => 'mypage#update'
+  get '/mypage/exhibit_list/list' => 'exhibit_list#list'
+  get '/mypage/exhibit_list/dealing' => 'exhibit_list#dealing'
+  get '/mypage/exhibit_list/complete' => 'exhibit_list#complete'
 
   resources :personal_informations, only: [:new, :create, :edit, :update]
 
   get '/signup' => 'signup#index'
   get '/logout' => 'logout#log_out'
+
   
 end
